1d7658e85dc43af11335c9a451247d9a8028da4d
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-06 23:51:46 +0100

    Display basic app information on startup

---
+++ src/helpers/terminal.ts
@@ -0,0 +1,21 @@
+import applicationConfig from '../config/application.config';
+import environmentConfig from '../config/environment.config';
+
+export function terminal(): void {
+  const appConfig = applicationConfig();
+  const envConfig = environmentConfig();
+  const route = () => `http://${appConfig.host}:${appConfig.port}`;
+  console.log(``);
+  console.log(
+    `Hello 👋, your app is ready on ${route()}${appConfig.routePrefix}`,
+  );
+  console.log(`To shut it down, press <CTRL> + C at any time.`);
+  console.log(``);
+  console.log(`------------------------------------------------------------`);
+  console.log(`Environment    : ${envConfig.node}`);
+  console.log(`Version        : ${appConfig.version}`);
+  console.log(``);
+  console.log(`API            : ${route()}/${appConfig.routePrefix}`);
+  console.log(`------------------------------------------------------------`);
+  console.log(``);
+}
