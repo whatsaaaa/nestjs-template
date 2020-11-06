43733a12b24821a08ebda0edea7cc4e63ee23ec9
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-06 23:02:50 +0100

    Delete logger and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import env from './configuration/env';
-
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [env],
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-import { LoggerService } from './logger/logger.service';
-import env from './configuration/env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: new LoggerService(),
-  });
-  await app.listen(env().app.port, env().app.host, () => {
-    console.log(
-      `Application started listening to ${env().app.host}:${
-        env().app.port
-      } in ${env().node} environment`,
-    );
-  });
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
