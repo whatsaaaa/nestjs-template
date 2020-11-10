d7d17c1495db0dde9dc71e94c9130db73d1be5b5
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-10 14:28:29 +0100

    Typeorm with an example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module, Logger } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { LoggerModule } from './modules/logger/logger.module';
 
@@ -8,14 +9,26 @@ import { HeroesGameModule } from './modules/heroes/heroes.module';
 import applicationConfig from './config/application.config';
 import environmentConfig from './config/environment.config';
 import databaseConfig from './config/database.config';
+import { UsersModule } from './modules/user/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [applicationConfig, environmentConfig, databaseConfig],
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'USERNAME',
+      password: 'PASSWORD',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     LoggerModule,
     HeroesGameModule,
+    UsersModule,
   ],
   providers: [Logger],
 })
